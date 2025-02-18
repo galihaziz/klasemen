ebc8bcc25eea040afe8bbd114a0b0fb181be2937
galihaziz <galihaziz234@gmail.com>
2025-02-18 16:26:20 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('interactive-img').addEventListener('click', function() {
+    alert('Image clicked!');
+});
+document.getElementById('interactive-table').addEventListener('click', function() {
+    alert('text clicked!');
+});
